49e170c30ff101a6ae5f8a9347529aec5ce0b948
FoxeyesJK <jakej.kwon@gmail.com>
2019-08-24 15:14:12 -0400

    Add initial router setup for Admin Login

--- ARank-Backend/src/api/index.js
+++ ARank-Backend/src/api/index.js
@@ -1,8 +1,10 @@
 const Router = require('koa-router');
 const artists = require('./artists');
+const auth = require('./auth');
 
 const api = new Router();
 
 api.use('/artists', artists.routes());
+api.use('/auth', auth.routes());
 
 module.exports = api;
