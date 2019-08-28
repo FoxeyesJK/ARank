773f2d2cffb290731aa4eb30944b2f2fa0d49b61
FoxeyesJK <jakej.kwon@gmail.com>
2019-08-27 22:06:14 -0400

    Add localStorage Login

--- ARank-Frontend/src/components/modal/LoginModal/LoginModal.js
+++ ARank-Frontend/src/components/modal/LoginModal/LoginModal.js
@@ -11,27 +11,30 @@ const LoginModal = ({
     visible, password, error, onCancel, onLogin, onChange, onKeyPress
 }) => (
     <ModalWrapper visible={visible} style={style.LoginModal}>
-          <Grid textAlign='center' style={{ height: '60vh' }} verticalAlign='middle'>
-    <Grid.Column style={{ maxWidth: 450 }}>
-      <Header as='h2' color='teal' textAlign='center'>
-        <Image src='/logo.png' /> Log-in to your account
-      </Header>
-      <Form size='large'>
-        <Segment stacked>
-          <Form.Input fluid icon='user' iconPosition='left' placeholder='E-mail address' />
-          <Form.Input
-            fluid
-            icon='lock'
-            iconPosition='left'
-            placeholder='Password'
-            type='password'
-          />
-
-          <Button color='teal' fluid size='large'>
-            Login
-          </Button>
-        </Segment>
-      </Form>
+        <Grid textAlign='center' style={{ height: '60vh' }} verticalAlign='middle'>
+            <Grid.Column style={{ maxWidth: 450 }}>
+                <Header as='h2' color='teal' textAlign='center'>
+                    <Image src='/logo.png' /> Log-in to your account
+                </Header>
+                <Form size='large'>
+                    <Segment stacked>
+                    <Form.Input fluid icon='user' iconPosition='left' placeholder='E-mail address' />
+                    <Form.Input
+                        fluid
+                        icon='lock'
+                        iconPosition='left'
+                        placeholder='Password'
+                        type='password'
+                        value={password}
+                        onChange={onChange}
+                        onKeyPress={onKeyPress}
+                    />
+                    { error && <div>Login Fail</div>}
+                    <Button onClick={onLogin} color='teal' fluid size='large'>
+                        Login
+                    </Button>
+                    </Segment>
+                </Form>
       <Message>
         New to us? <a href='#'>Sign Up</a>
       </Message>
--- ARank-Frontend/src/containers/common/Base.js
+++ ARank-Frontend/src/containers/common/Base.js
@@ -6,7 +6,11 @@ import * as baseActions from '../../store/modules/base';
 
 class Base extends Component {
     intialize = async () => {
-        // Login Status Check 
+        const { BaseActions } = this.props;
+        if(localStorage.logged === "true") {
+            BaseActions.tempLogin();
+        }
+        BaseActions.checkLogin();
     }
     componentDidMount() {
         this.intialize();
